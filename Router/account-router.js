fc09c72171a6cb95f25c17a247cd0510fea70c85
irisjitomo <irisjitomo@eyahoo.com>
2019-10-07 16:58:43 -0400

    finished CRUD and ran npx knex seed:run

--- Router/account-router.js
+++ Router/account-router.js
@@ -5,8 +5,8 @@ const db = require('../data/dbConfig');
 const router = express.Router();
 
 router.post('/', (req, res) => {
-    const accData = req.body;
-    (!accData.name || !accData.budget)
+	const accData = req.body;
+	!accData.name || !accData.budget
 		? res.status(400).json({ error: 'Include name and budget' })
 		: db('accounts')
 				.insert(accData, 'id')
@@ -32,15 +32,45 @@ router.get('/:id', (req, res) => {
 	const id = req.params.id;
 	db
 		.select('*')
-		.from('posts')
-		.where('id', id)
+		.from('accounts')
+		.where('id', '=', id)
+		.first()
 		.then((account) => {
-			res.status(200).json(account);
+			!account ? res.status(404).json({ error: 'id does not exist' }) : res.json(account);
 		})
 		.catch((err) => {
 			res.status(500).json(err);
 		});
 });
 
+router.delete('/:id', (req, res) => {
+	const id = req.params.id;
+	db('accounts')
+		.where('id', '=', id)
+		.del()
+		.then((account) => {
+			!account ? res.status(400).json({ error: 'id does not exist' }) : res.status(200).json(account);
+		})
+		.catch(() => {
+			res.status(500).json({ error: 'could not delete' });
+		});
+});
+
+router.put('/:id', (req, res) => {
+	const update = req.body;
+	const id = req.params.id;
+	!update.name || !update.budget
+		? res.status(400).json({ error: 'Include name and budget' })
+		: db('accounts')
+				.where({ id })
+				.update(update)
+				.then((account) => {
+                    (!account) ? res.status(400).json({ error: 'id does not exist' }) :
+					res.status(200).json(account);
+				})
+				.catch((err) => {
+					res.status(500).json(err);
+				});
+});
 
 module.exports = router;
